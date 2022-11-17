ebc3d33e47610061187b3ae8395b7464f813183e
kiksgold <kiksgold@gmail.com>
2022-11-17 22:34:36 +0000

    Display book_detail view

--- library/urls.py
+++ library/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.BookList.as_view(), name='home'),
+    path('<slug:slug>/', views.BookDetail.as_view(), name='book_detail'),
 ]
--- library/views.py
+++ library/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Book
 
 
@@ -8,3 +8,24 @@ class BookList(generic.ListView):
     queryset = Book.objects.all().order_by('-uploaded_on')
     template_name = 'index.html'
     paginate_by = 6
+
+
+class BookDetail(View):
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Book.objects.filter()
+        book = get_object_or_404(queryset, slug=slug)
+        reviews = book.reviews.filter(approved=True).order_by('uploaded_on')
+        liked = False
+        if book.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "book_detail.html",
+            {
+                "book": book,
+                "reviews": reviews,
+                "liked": liked,
+
+            }
+        )
